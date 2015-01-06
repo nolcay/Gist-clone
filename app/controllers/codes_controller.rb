3baaf4e50aa06b844ad4f7d6360b44f507bdbabf
nolcay <olcay.oransoy@gmail.com>
2015-01-06 12:19:40 +0200

    Third and first bonus step complete

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,5 +1,5 @@
 class CodesController < ApplicationController
-  before_action :set_code, only: [:show, :edit, :update, :destroy]
+  before_action :set_code, only: [:show]
 
   # GET /codes
   # GET /codes.json
@@ -17,10 +17,6 @@ class CodesController < ApplicationController
     @code = Code.new
   end
 
-  # GET /codes/1/edit
-  def edit
-  end
-
   # POST /codes
   # POST /codes.json
   def create
@@ -35,30 +31,6 @@ class CodesController < ApplicationController
         format.html { render :new }
         format.json { render json: @code.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /codes/1
-  # PATCH/PUT /codes/1.json
-  def update
-    respond_to do |format|
-      if @code.update(code_params)
-        format.html { redirect_to @code, notice: 'Code was successfully updated.' }
-        format.json { render :show, status: :ok, location: @code }
-      else
-        format.html { render :edit }
-        format.json { render json: @code.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /codes/1
-  # DELETE /codes/1.json
-  def destroy
-    @code.destroy
-    respond_to do |format|
-      format.html { redirect_to codes_url, notice: 'Code was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
